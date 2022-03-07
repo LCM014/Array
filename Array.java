9f7650c5cecfd18671e894ba991c69795fdabfb9
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-03-07 19:55:05 +0000

    salvando

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array
+{
+    public static void main(String[] args) 
+    {
+        int [] Array = {1,2,3,4,5};
+
+        for(int i : Array)
+        {
+            System.out.println(i);
+        }
+    }
+}
